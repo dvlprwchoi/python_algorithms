4447e1147676fd4643a04e92dfbb6b6d2b4d30ee
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-10-14 23:44:21 -0500

    Complete fizzbuzz function

---
+++ fizzbuzz/main.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    for i in range(1, num+1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizzbuzz(100)
